8b6d1fc0061e69962dcfb6f26bcfcd80baba7f03
Aliaksei Urbanski <aliaksei.urbanski@itechart-group.com>
2015-05-27 22:13:20 +0300

    Functional programming, problem 6 solved

---
+++ Functional programming/Problem 6/filter.js
@@ -0,0 +1,18 @@
+function Filter(f, array) {
+	"use strict";
+
+	var new_array = [];
+	for (var index = 0; index < array.length; index++) {
+		var element = array[index];
+		if (f(element))
+			new_array.push(element);
+	}
+
+	return new_array;
+}
+
+function nativeFilter(f, array) {
+	"use strict";
+
+	return array.filter(f);
+}
